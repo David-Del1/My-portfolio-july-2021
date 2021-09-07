ca712b7fa0604a432667d24dc9de1eeac1b2772c
David Delgadillo <daviddel.731@gmail.com>
2021-09-06 19:51:27 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ function App() {
   return (
   <div>
       <MainContentStyled>
-        <div className="lines">
-
-          
-        </div>
           <Home />
       </MainContentStyled>
   </div>
@@ -24,23 +20,6 @@ const MainContentStyled = styled.main`
   position: relative;
   min-height: 100vh;
 
-  .lines {
-    position: absolute;
-    min-height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    opacity: 0.5;
-    z-index: -1;
-    .line-1, .line-2, .line-3, .line-4 {
-      width: 1px;
-      min-height: 100vh;
-      background-color: var(--border-color);
-      opacity: 0.5;
-    }
-  }
-
-  
   @media only screen and (max-width: 1100px) {
     margin-left: 0;
   }

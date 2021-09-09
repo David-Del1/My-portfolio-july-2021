acd746cbd3066520f04537c9fa2f927bf8bb51fc
David Delgadillo <daviddel.731@gmail.com>
2021-09-08 22:53:07 -0700

    took off app provider in index file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import AppProvider from './state/AppProvider';
 import GlobalStyle from './styles/GlobalStyles.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
-    <AppProvider>
       <App />
-    </AppProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
